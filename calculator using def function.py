63de42c5771edbc9490a7024200040a2fdaa715e
Diksha <ariadiksha23@gmail.com>
2025-07-17 21:08:15 +0530

    Update calculator using def function.py

--- calculator using def function.py
+++ calculator using def function.py
@@ -1,40 +1,39 @@
-def add(x,y):
-    return x+y
+# Define functions for calculator
+def add(x, y):
+    return x + y
 
-def sub(x,y):
-    return x-y
+def sub(x, y):
+    return x - y
 
-def mul(x,y):
-    return x*y
+def mul(x, y):
+    return x * y
 
-def div(x,y):
-    return x/y
+def div(x, y):
+    return x / y
 
+# User options that should be performes
 print("""Enter operation to be performed
-        1 for add
-        2 for sub
-        3 for mul
-        4 for div""" )
+1 for add
+2 for sub
+3 for mul
+4 for div""" )
 
+# Take user input
+d = input("Enter your choice--> ")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
-d=input("Enter your choice-->")
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-
-
-
-if d == add:
-    print("Addition of two numbers:", x+y)
-elif d == sub:
-    print("Subtraction of two numbers:", x-y)
-elif d == mul:
-    print("Multiplication of two numbers:", x*y)
-elif d== div:
-    if i != 0:
-        print("Division of two numbers:", x/y)
+# function calling
+if d == "1":
+    print("Addition of two numbers:", add(a, b))
+elif d == "2":
+    print("Subtraction of two numbers:", sub(a, b))
+elif d == "3":
+    print("Multiplication of two numbers:", mul(a, b))
+elif d == "4":
+    if b != 0:
+        print("Division of two numbers:", div(a, b))
     else:
         print("Cannot divide by zero!")
-
 else:
     print("You entered a wrong operation!!!")
-
